38b5104a2fa81fe084b2e398aad3296ad11844c2
Maintenance Web <maintenance.web@gmail.com>
2026-04-25 04:40:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4496543405-8b79d",
+  "appId": "1:198651806690:web:61d3f29c725bef0b4a2fbb",
+  "apiKey": "AIzaSyAuL46IIHC0h2EaPW9dcN0hhPrwm-KTfLw",
+  "authDomain": "studio-4496543405-8b79d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "198651806690"
+};
